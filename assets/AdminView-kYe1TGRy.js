81e226c8c6608b1f2e942a9355e16ee1eefb02c4
Diego Vite Hernández <undecode0@hotmail.com>
2024-10-18 20:05:14 -0600

    Home path: changed

---
+++ assets/AdminView-kYe1TGRy.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,h as a,o}from"./index-Df20Fvi5.js";const s={};function n(c,e){return o(),t("div",null,e[0]||(e[0]=[a("h1",{class:"text-4xl"},"Vista privada de administrador",-1)]))}const l=r(s,[["render",n]]);export{l as default};
