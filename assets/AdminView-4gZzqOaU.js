092e89ab9620ad13af7e8ee18e54a89bc9489a03
Diego Vite Hernández <undecode0@hotmail.com>
2024-10-24 10:33:55 -0600

    Auth: Finished

--- assets/AdminView-4gZzqOaU.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as a,h as r,o as s}from"./index-DFWwSDKG.js";const n={};function o(c,e){return s(),a("div",null,e[0]||(e[0]=[r("h1",{class:"text-4xl"},"Vista privada de administrador",-1)]))}const i=t(n,[["render",o]]);export{i as default};
---
+++ assets/undraw_mailbox_re_dvds-D0cFtK3L.js
@@ -0,0 +1,1 @@
+const s="/assets/undraw_mailbox_re_dvds-q0CWimCl.svg";export{s as _};
