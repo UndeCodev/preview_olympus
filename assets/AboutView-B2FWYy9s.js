050c52577319eca37dc358f043f26ca0c268d47b
Diego Vite Hernández <undecode0@hotmail.com>
2024-10-24 11:58:31 -0600

    Lat updated

--- assets/AboutView-B2FWYy9s.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,g as c,h as s,o as n}from"./index-CIPrjnkA.js";const o={},r={class:"h-screen place-content-center text-center"};function a(l,e){return n(),c("div",r,e[0]||(e[0]=[s("h1",{class:"text-6xl"},"Acerca de Olympus GYM",-1)]))}const d=t(o,[["render",a]]);export{d as default};
--- assets/ProfilePage-Dbve6DRb.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,g as o,h as t,o as s}from"./index-CIPrjnkA.js";const a={};function n(c,e){return s(),o("div",null,e[0]||(e[0]=[t("h1",{class:"text-red-300"},"Profile",-1)]))}const f=r(a,[["render",n]]);export{f as default};
